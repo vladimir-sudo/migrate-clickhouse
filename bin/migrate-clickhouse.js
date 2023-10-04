d13b5c133761edcfb2310f1cdfd2b3b8aeecaef8
Vladimir Malec <vladimirmalec@gmail.com>
2023-10-04 18:55:15 +0300

    fix

--- bin/migrate-clickhouse.js
+++ bin/migrate-clickhouse.js
@@ -7,121 +7,121 @@ const migrateClickhouse = require("../lib/migrate-clickhouse");
 const pkgjson = require("../package.json");
 
 function printMigrated(migrated = []) {
-  migrated.forEach(migratedItem => {
-    console.log(`MIGRATED UP: ${migratedItem}`);
-  });
+    migrated.forEach(migratedItem => {
+        console.log(`MIGRATED UP: ${migratedItem}`);
+    });
 }
 
 function handleError(err) {
-  console.error(`ERROR: ${err.message}`, err.stack);
-  process.exit(1);
+    console.error(`ERROR: ${err.message}`, err.stack);
+    process.exit(1);
 }
 
 function printStatusTable(statusItems) {
-  return migrateClickhouse.config.read().then(config => {
-    const useFileHash = config.useFileHash === true;
-    const table = new Table({ head: useFileHash ? ["Filename", "Hash", "Applied At"] : ["Filename", "Applied At"]});
-    statusItems.forEach(item => table.push(_.values(item)));
-    console.log(table.toString());
-  })
-  
+    return migrateClickhouse.config.read().then(config => {
+        const useFileHash = config.useFileHash === true;
+        const table = new Table({head: useFileHash ? ["Filename", "Hash", "Applied At"] : ["Filename", "Applied At"]});
+        statusItems.forEach(item => table.push(_.values(item)));
+        console.log(table.toString());
+    })
+
 }
 
 program.version(pkgjson.version);
 
 program
-  .command("init")
-  .description("initialize a new migration project")
-  .option("-m --module <module loading system>", "module loading system (commonjs (DEFAULT) or esm)")
-  .action(options => {
-    global.options = options;
-    migrateClickhouse
-      .init()
-      .then(() =>
-        console.log(
-          `Initialization successful. Please edit the generated \`${migrateClickhouse.config.getConfigFilename()}\` file`
-        )
-      )
-      .catch(err => handleError(err))
-  });
+    .command("init")
+    .description("initialize a new migration project")
+    .option("-m --module <module loading system>", "module loading system (commonjs (DEFAULT) or esm)")
+    .action(options => {
+        global.options = options;
+        migrateClickhouse
+            .init()
+            .then(() =>
+                console.log(
+                    `Initialization successful. Please edit the generated \`${migrateClickhouse.config.getConfigFilename()}\` file`
+                )
+            )
+            .catch(err => handleError(err))
+    });
 
 program
-  .command("create [description]")
-  .description("create a new database migration with the provided description")
-  .option("-f --file <file>", "use a custom config file")
-  .action((description, options) => {
-    global.options = options;
-    migrateClickhouse
-      .create(description)
-      .then(fileName => 
-        migrateClickhouse.config.read().then(config => {
-          console.log(`Created: ${config.migrationsDir}/${fileName}`);
-        })
-      )
-      .then(() => {
-        process.exit(0);
-      })
-      .catch(err => handleError(err));
-  });
+    .command("create [description]")
+    .description("create a new database migration with the provided description")
+    .option("-f --file <file>", "use a custom config file")
+    .action((description, options) => {
+        global.options = options;
+        migrateClickhouse
+            .create(description)
+            .then(fileName =>
+                migrateClickhouse.config.read().then(config => {
+                    console.log(`Created: ${config.migrationsDir}/${fileName}`);
+                })
+            )
+            .then(() => {
+                process.exit(0);
+            })
+            .catch(err => handleError(err));
+    });
 
 program
-  .command("up")
-  .description("run all pending database migrations")
-  .option("-f --file <file>", "use a custom config file")
-  .action(options => {
-    global.options = options;
-    migrateClickhouse.database
-      .connect()
-      .then(({db, client}) => migrateClickhouse.up(db, client))
-      .then(migrated => {
-        printMigrated(migrated);
-        process.exit(0);
-      })
-      .catch(err => {
-        handleError(err);
-        printMigrated(err.migrated);
-      });
-  });
+    .command("up")
+    .description("run all pending database migrations")
+    .option("-f --file <file>", "use a custom config file")
+    .action(options => {
+        global.options = options;
+        migrateClickhouse.database
+            .connect()
+            .then(({db, client}) => migrateClickhouse.up(db, client))
+            .then(migrated => {
+                printMigrated(migrated);
+                process.exit(0);
+            })
+            .catch(err => {
+                handleError(err);
+                printMigrated(err.migrated);
+            });
+    });
 
 program
-  .command("down")
-  .description("undo the last applied database migration")
-  .option("-f --file <file>", "use a custom config file")
-  .action(options => {
-    global.options = options;
-    migrateClickhouse.database
-      .connect()
-      .then(({db, client}) => migrateClickhouse.down(db, client))
-      .then(migrated => {
-        migrated.forEach(migratedItem => {
-          console.log(`MIGRATED DOWN: ${migratedItem}`);
-        });
-        process.exit(0);
-      })
-      .catch(err => {
-        handleError(err);
-      });
-  });
+    .command("down")
+    .description("undo the last applied database migration")
+    .option("-f --file <file>", "use a custom config file")
+    .action(options => {
+        global.options = options;
+        migrateClickhouse.database
+            .connect()
+            .then(({db, client}) => migrateClickhouse.down(db, client))
+            .then(migrated => {
+                migrated.forEach(migratedItem => {
+                    console.log(`MIGRATED DOWN: ${migratedItem}`);
+                });
+                process.exit(0);
+            })
+            .catch(err => {
+                handleError(err);
+            });
+    });
 
 program
-  .command("status")
-  .description("print the changelog of the database")
-  .option("-f --file <file>", "use a custom config file")
-  .action(options => {
-    global.options = options;
+    .command("status")
+    .description("print the changelog of the database")
+    .option("-f --file <file>", "use a custom config file")
+    .action(options => {
+        global.options = options;
 
-      migrateClickhouse.status(migrateClickhouse.database)
-          .then(statusItems => printStatusTable(statusItems))
-          .then(() => {
-              process.exit(0);
-          })
-          .catch(err => {
-              handleError(err);
-          });
-  });
+        migrateClickhouse.status(migrateClickhouse.database)
+            .then(statusItems => printStatusTable(statusItems))
+            .then(() => {
+                process.exit(0);
+            })
+            .catch(err => {
+                handleError(err);
+            });
+    });
 
 program.parse(process.argv);
 
 if (_.isEmpty(program.rawArgs)) {
-  program.outputHelp();
+    program.outputHelp();
 }
